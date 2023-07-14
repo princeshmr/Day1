964c40d055a7bb160eca273f2ac5d29f89bf5ad1
rohit789@ <princeshmr27@gmail.com>
2023-07-14 14:07:09 +0530

    Display Default value of primitive data

---
+++ DefValPrimitive.java
@@ -0,0 +1,22 @@
+class DefValPrimitive
+{
+	static byte num1;
+	static short num2;
+	static int num3;
+	static long num4;
+	static String val1;
+	static float num5;
+	static double num6;
+	static boolean val2;
+        public static void main(String[] args) {
+	   
+              System.out.println("Default value of byte is  = " + num1);
+              System.out.println("Default value of short is = " + num2);
+              System.out.println("Default value of int is = " + num3);
+              System.out.println("Default value of long is = " + num4);
+              System.out.println("Default value of String is  = " + val1);
+              System.out.println("Default value of float is = " + num5);
+	      System.out.println("Default value of double is = " + num6);
+	      System.out.println("Default value of boolean is = " + val2);
+   }
+}

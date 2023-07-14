9c8149d324e37f05a1802aeb0f97342c7ffbb113
rohit789@ <princeshmr27@gmail.com>
2023-07-14 14:09:18 +0530

    Strings Equals

---
+++ CheckStringEqual.java
@@ -0,0 +1,16 @@
+class CheckStringEqual 
+{
+	public static void main(String[] args) 
+	{
+		String s1="Rohit";
+		String s2="Rohit";
+		if (s1.equals(s2)== true)
+		{
+			System.out.println("Strings are equals ");
+		}
+		else
+		{
+			System.out.println("Strings are not equals");
+		}
+	}
+}

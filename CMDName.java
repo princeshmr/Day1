d618ef404f514c909a7df93e0c6c29cc19bd615e
rohit789@ <princeshmr27@gmail.com>
2023-07-14 14:20:47 +0530

    getting the name using CMD

---
+++ CMDName.java
@@ -0,0 +1,9 @@
+class CMDName 
+{
+	public static void main(String[] args) 
+	{
+                for(int i=0;i<args.length;i++){
+		        System.out.println("Your name is: "+args[i]);
+                }
+	}
+}
